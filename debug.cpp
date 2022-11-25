6548cd399fda6e9ce35fd6204ae112e3695855e8
Siddhant Agarwal <agarwalsiddhant53@gmail.com>
2022-11-25 11:08:49 +0530

    Debugging file written by tourist, modified by Aaron He

---
+++ debug.cpp
@@ -0,0 +1,153 @@
+// debug file written by tourist, modified by Aaron He
+// https://github.com/AaronHe7/competitive-programming/blob/master/algorithms/debug.cpp
+
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+
+// Helper functions
+template <typename A>
+string to_csv(vector<A> v);
+
+string wrap_round(string s) {
+	return "(" + s + ")";
+}
+
+string wrap_curly(string s) {
+	return "{" + s + "}";
+}
+
+string wrap_square(string s) {
+	return "[" + s + "]";
+}
+
+string wrap_angle(string s) {
+	return "<" + s + ">";
+}
+
+// Main functions
+string to_string(string s) {
+  return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+string to_string(vector<bool> v) {
+	return wrap_curly(to_csv(v));
+}
+
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+template <typename A>
+string to_string(vector<vector<A>> v) {
+  bool first = true;
+  string res = "\n{";
+  for (const auto x : v) {
+    if (!first) {
+      res += ", ";
+			res += "\n";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+template <typename A>
+string to_string(queue<A> q) {
+	vector<A> v;
+	while (!q.empty()) {
+		v.push_back(q.front());
+		q.pop();
+	}
+	string res = "<=" + wrap_curly(to_csv(v)) + "<=";
+	return res;
+}
+
+template <typename A>
+string to_string(deque<A> dq) {
+	vector<A> v;
+	while (!dq.empty()) {
+		v.push_back(dq.front());
+		dq.pop_front();
+	}
+	string res = "<=>" + wrap_curly(to_csv(v)) + "<=>";
+	return res;
+}
+
+template <typename A>
+string to_string(stack<A> s) {
+	vector<A> v;
+	while (!s.empty()) {
+		v.push_back(s.top());
+		s.pop();
+	}
+	string res = "<=>" + wrap_curly(to_csv(v));
+	return res;
+}
+template <typename A, typename B, typename C>
+string to_string(priority_queue<A, B, C> q) {
+	vector<A> v;
+	while (!q.empty()) {
+		v.push_back(q.top());
+		q.pop();
+	}
+	string res = "<=>>" + wrap_curly(to_csv(v));
+	return res;
+}
+
+template <typename A>
+string to_csv(vector<A> v) {
+	string res;
+	bool first = true;
+	for (A x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	return res;
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head>
+void debug_out(Head H) {
+	cerr << to_string(H) << endl;
+}
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << to_string(H) << ", ";
+  debug_out(T...);
+}
+
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
